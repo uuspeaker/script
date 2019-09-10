aeac90bdf36c1e4fad5de0abfe1e12c5ea2c58d0
tianhan <63253294@qq.com>
2019-09-10 17:07:00 +0800

    1

--- mongo/util.sh
+++ mongo/util.sh
@@ -9,4 +9,3 @@ db.createUser({
 db.KnowledgeTree.find().forEach(function(d){ db.getSiblingDB('vue_study')['KnowledgeTree'].insert(d); });
 #修改答案,用于测试
 db.XkwSubject.update({'key':'ba15d7af1561b871e77bee0ebb24d814'},{$set:{ 'questionContent':['.所以就很难定义什么是“人工”制造的“智能”了。因此人工智能的研究往往涉及对人的', '智能本身的研究。其它关于动物或其它人造系统的智能也普遍被认为是人工智能相关的研','究课题',], 'answer': 0, 'questionType': 2}})
-db.XkwSubject.update({'key':'ba15d7af1561b871e77bee0ebb24d814'},{$set:{ 'questionContent':'所以就很难定义什么是“人工”制造的“智能”了。因此人工智能的研究往往涉及对人的, 智能本身的研究。其它关于动物或其它人造系统的智能也普遍被认为是人工智能相关的研,究课题', 'answer': 0}})
