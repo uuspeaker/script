6f15f3628c0a198c101334aadfc98b32b7a853d5
tianhan <63253294@qq.com>
2019-10-17 15:07:42 +0800

    11

---
+++ tensorflow/util.sh
@@ -0,0 +1,4 @@
+mkdir ~/tensorflow
+docker run --name tensorflow -it -p 8888:8888 -v ~/tensorflow:/test/data jupyter/tensorflow-notebook
+http://129.211.21.250/?token=aba07c227991a64a666172fd900911061c1a03ee9d332926
+docker logs 
